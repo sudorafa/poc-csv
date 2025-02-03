482cadfd3da79613899a87cb4dcb177053322ea0
Rafael Eduardo Lima <6751801+sudorafa@users.noreply.github.com>
2025-02-03 11:10:49 -0300

    add integration test for check values in CSV data without mock;

--- src/tests/integration.test.js
+++ src/tests/integration.test.js
@@ -2,6 +2,7 @@ const request = require("supertest");
 const app = require("../app");
 const { sequelize } = require("../models");
 const { insertMovies } = require("../controllers/movieController");
+const { csvParser } = require("../utils/csvParser");
 
 describe("GET /api/intervals - Data Consistency with file", () => {
   beforeAll(async () => {
@@ -13,7 +14,7 @@ describe("GET /api/intervals - Data Consistency with file", () => {
     await sequelize.close();
   });
 
-  test("Should return results that match with original file", async () => {
+  test("Should return results that match with mock", async () => {
     const res = await request(app).get("/api/intervals");
     expect(res.status).toBe(200);
     expect(res.body).toHaveProperty("min");
@@ -35,5 +36,50 @@ describe("GET /api/intervals - Data Consistency with file", () => {
 
     expect(min).toContainEqual(expectedMin);
     expect(max).toContainEqual(expectedMax);
+  });
+
+  test("Should return results that match with original file csv", async () => {
+    const res = await request(app).get("/api/intervals");
+    csvData = await csvParser();
+    const { min, max } = res.body;
+
+    const transformedCSVData = csvData.map((row) => {
+      return {
+        title: row.title.trim(),
+        year: parseInt(row.year.trim(), 10),
+        studios: row.studios.split(",").map((studio) => studio.trim()),
+        producer: row.producers
+          .split(/,| and /)
+          .map((producer) => producer.trim()),
+        winner: row.winner.trim().toLowerCase() === "yes",
+      };
+    });
+
+    const checkProducerInCSV = (producer, year) => {
+      return transformedCSVData.some(
+        (row) =>
+          row.producer.includes(producer) &&
+          row.year === parseInt(year, 10) &&
+          row.winner
+      );
+    };
+
+    min.forEach((interval) => {
+      expect(checkProducerInCSV(interval.producer, interval.previousWin)).toBe(
+        true
+      );
+      expect(checkProducerInCSV(interval.producer, interval.followingWin)).toBe(
+        true
+      );
+    });
+
+    max.forEach((interval) => {
+      expect(checkProducerInCSV(interval.producer, interval.previousWin)).toBe(
+        true
+      );
+      expect(checkProducerInCSV(interval.producer, interval.followingWin)).toBe(
+        true
+      );
+    });
   });
 });
