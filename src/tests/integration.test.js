4afdd759cd5f488a2af3efa4067fafdd650d82d8
Rafael Eduardo Lima <6751801+sudorafa@users.noreply.github.com>
2025-02-03 10:04:55 -0300

    fix integration tests for data consistency with file; remove CSV file dependency and update assertions

--- src/tests/integration.test.js
+++ src/tests/integration.test.js
@@ -2,30 +2,10 @@ const request = require("supertest");
 const app = require("../app");
 const { sequelize } = require("../models");
 const { insertMovies } = require("../controllers/movieController");
-const fs = require("fs");
-const path = require("path");
-const { csvParser } = require("../utils/csvParser");
 
-describe("GET /api/intervals", () => {
+describe("GET /api/intervals - Data Consistency with file", () => {
   beforeAll(async () => {
     await sequelize.sync({ force: true });
-
-    const testCsvPath = path.join(__dirname, "test-data.csv");
-    fs.writeFileSync(
-      testCsvPath,
-      "year;title;studios;producers;winner\n" +
-        "1980;Movie A;Studio 1;Producer A, Producer B;yes\n" +
-        "1985;Movie B;Studio 2;Producer A;yes\n" +
-        "1990;Movie C;Studio 3;Producer B;yes\n" +
-        "2000;Movie D;Studio 4;Producer C;yes"
-    );
-
-    jest
-      .spyOn(require("../utils/csvParser"), "csvParser")
-      .mockImplementation(async () => {
-        return csvParser(testCsvPath);
-      });
-
     await insertMovies();
   });
 
@@ -33,12 +13,27 @@ describe("GET /api/intervals", () => {
     await sequelize.close();
   });
 
-  test("Should return the producers with the longest and shortest intervals between awards", async () => {
+  test("Should return results that match with original file", async () => {
     const res = await request(app).get("/api/intervals");
     expect(res.status).toBe(200);
     expect(res.body).toHaveProperty("min");
     expect(res.body).toHaveProperty("max");
-    expect(res.body.min.length).toBeGreaterThan(0);
-    expect(res.body.max.length).toBeGreaterThan(0);
+    const { min, max } = res.body;
+
+    const expectedMin = {
+      producer: "Joel Silver",
+      interval: 1,
+      previousWin: 1990,
+      followingWin: 1991,
+    };
+    const expectedMax = {
+      producer: "Matthew Vaughn",
+      interval: 13,
+      previousWin: 2002,
+      followingWin: 2015,
+    };
+
+    expect(min).toContainEqual(expectedMin);
+    expect(max).toContainEqual(expectedMax);
   });
 });
